2cef4f1c33d073e3a7f4e12ecddb248c2ac7c3d3
James Andrews <jgandrews@gmail.com>
2022-12-12 22:22:42 +0000

    Puzzle 3 - Part 2

--- Puzzle2/README.md
+++ Puzzle2/README.md
@@ -1,3 +1,5 @@
-# Advent of Code 2022 Day 1 Puzzle
+# Advent of Code 2022 Day 2 Puzzle
 
 [Puzzle 2](https://adventofcode.com/2022/day/2)
+
+There is a way better way of doing this one, but this was what I did on my first attempt.
--- Puzzle2/main.go
+++ Puzzle2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,7 +17,8 @@ func main() {
 		return
 	}
 
-	totalScore := 0
+	totalScorePart1 := 0
+	totalScorePart2 := 0
 	RPSReader := bufio.NewReader(RPSData)
 
 	// I dont like this loop. TODO: Do this another way
@@ -27,14 +29,17 @@ func main() {
 			break
 		}
 
-		totalScore = totalScore + scoreGame(string(line))
+		totalScorePart1 = totalScorePart1 + scoreGame(string(line), 1)
+		totalScorePart2 = totalScorePart2 + scoreGame(string(line), 2)
 	}
 
-	fmt.Printf("The total score following the strategy was: %d\n", totalScore)
+	fmt.Printf("\nThe total score following the strategy for part 1 was: %d\n", totalScorePart1) // 12586
+
+	fmt.Printf("The total score following the strategy for part 2 was: %d\n", totalScorePart2)
 
 }
 
-func scoreGame(gameData string) int {
+func scoreGame(gameData string, mode int) int {
 	// A,X = Rock
 	// B,Y = Paper
 	// C,Z = Scissors
@@ -56,7 +61,36 @@ func scoreGame(gameData string) int {
 	playerRPS["Z"] = "Scissors"
 
 	opponent := opponentRPS[string(gameData[0])]
-	player := playerRPS[string(gameData[2])]
+	player := ""
+
+	if mode == 1 {
+		player = playerRPS[string(gameData[2])]
+
+	} else {
+		// X = Lose
+		// Y = Draw
+		// Z = Win
+
+		// Make a Draw
+		if string(gameData[2]) == "Y" {
+			player = opponent
+			fmt.Printf("Opponent: %s, Player: %s. Should be a DRAW\n", opponent, player)
+		}
+
+		// Make a Loss
+		if string(gameData[2]) == "X" {
+
+			player = chooseStrat(opponent, 0)
+			fmt.Printf("Opponent: %s, Player: %s. Should be a LOSS\n", opponent, player)
+		}
+
+		// Make a Win
+		if string(gameData[2]) == "Z" {
+			player = chooseStrat(opponent, 1)
+			fmt.Printf("Opponent: %s, Player: %s. Should be a WIN\n", opponent, player)
+		}
+
+	}
 
 	score := ShapeValue[player]
 
@@ -93,4 +127,30 @@ func doesThePlayerWin(player string, opponent string) bool {
 	}
 
 	return winner
+}
+
+// 0 to pick a losing shape, anything else to pick a winning shape
+func chooseStrat(opponentShape string, winlose int) string {
+	shape := ""
+
+	// This is super ugly! TODO: Fix!
+	if strings.Compare(opponentShape, "Rock") == 0 && winlose == 0 {
+		shape = "Scissors"
+	} else if strings.Compare(opponentShape, "Rock") == 0 && winlose != 0 {
+		shape = "Paper"
+	}
+
+	if strings.Compare(opponentShape, "Paper") == 0 && winlose == 0 {
+		shape = "Rock"
+	} else if strings.Compare(opponentShape, "Paper") == 0 && winlose != 0 {
+		shape = "Scissors"
+	}
+
+	if strings.Compare(opponentShape, "Scissors") == 0 && winlose == 0 {
+		shape = "Paper"
+	} else if strings.Compare(opponentShape, "Scissors") == 0 && winlose != 0 {
+		shape = "Rock"
+	}
+
+	return shape
 }
