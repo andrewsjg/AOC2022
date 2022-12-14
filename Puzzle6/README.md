1792cd6950befd0970624ebe6194820ea2ab98f8
James Andrews <jgandrews@gmail.com>
2022-12-14 22:27:04 +0000

    Puzzle 6 - Part 2

--- Puzzle6/README.md
+++ Puzzle6/README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2022 Day 6 Puzzle
 
 [Day 5 Puzzle](https://adventofcode.com/2022/day/6)
+
+I felt like I might have hacked my way through this one. Making good use of the strings library! 
--- Puzzle6/main.go
+++ Puzzle6/main.go
@@ -17,29 +17,44 @@ func main() {
 	processDataStream(string(data))
 }
 
-// 1953
 func processDataStream(data string) {
 
 	firstMarker := 0
+	firstSoM := 0
 
 	for i := 0; i < len(data); i++ {
 
-		checkStr := ""
+		checkMarker := ""
+		checkSoM := ""
+
 		if i+3 < len(data) {
-			checkStr = string(data[i]) + string(data[i+1]) + string(data[i+2]) + string(data[i+3])
+			checkMarker = string(data[i]) + string(data[i+1]) + string(data[i+2]) + string(data[i+3])
 		}
 
-		if !hasDups(checkStr) {
-			//fmt.Printf("Marker: %s at %d\n", checkStr, (strings.Index(data, checkStr))+4)
+		if i+13 < len(data) {
+			checkSoM = string(data[i]) + string(data[i+1]) + string(data[i+2]) + string(data[i+3]) + string(data[i+4]) +
+				string(data[i+5]) + string(data[i+6]) + string(data[i+7]) + string(data[i+8]) + string(data[i+9]) +
+				string(data[i+10]) + string(data[i+11]) + string(data[i+12]) + string(data[i+13])
+		}
+
+		if !hasDups(checkMarker) {
 
 			if firstMarker == 0 {
-				firstMarker = (strings.Index(data, checkStr)) + 4
+				firstMarker = (strings.Index(data, checkMarker)) + 4
+			}
+		}
+
+		if !hasDups(checkSoM) {
+
+			if firstSoM == 0 {
+				firstSoM = (strings.Index(data, checkMarker)) + 14
 			}
 		}
 
 	}
 
 	fmt.Printf("First marker found at: %d\n", firstMarker)
+	fmt.Printf("First start of message found at: %d\n", firstSoM)
 }
 
 func hasDups(checkStr string) bool {
